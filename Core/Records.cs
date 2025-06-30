4ec437ce1e22f3fac1d306ca5f51abe6d137b24c
임세환 <limprovi@line.games>
2025-06-30 13:43:40 +0900

    1

--- Core/Records.cs
+++ Core/Records.cs
@@ -10,154 +10,148 @@ using Newtonsoft.Json;
 using Steamworks;
 #endif
 
-
-[Serializable]
-public class RecordValue
+public class Records
 {
-    public object persist;
-    public object session;
+    public object persist; // For persistent storage
+    public object session; // For current session
     public Type type;
-    public bool isHigh;
+    public bool ifHigh;
     public bool isSteam;
 
-    public bool IsHigh => isHigh;
-
-    public RecordValue(Type type, bool isHigh, bool isSteam)
+    public Records(Type type, bool ifHigh, bool isSteam)
     {
         this.type = type;
-        this.isHigh = isHigh;
+        this.ifHigh = ifHigh;
         this.isSteam = isSteam;
         persist = null;
         session = null;
     }
 
-    public void Operate(object value)
-    {
-        if (isHigh)
-        {
-            if (persist == null) persist = value;
-            else if (type == typeof(int)) persist = Math.Max(Convert.ToInt32(persist), Convert.ToInt32(value));
-            else if (type == typeof(long)) persist = Math.Max(Convert.ToInt64(persist), Convert.ToInt64(value));
-            else if (type == typeof(float)) persist = Math.Max(Convert.ToSingle(persist), Convert.ToSingle(value));
-            else if (type == typeof(double)) persist = Math.Max(Convert.ToDouble(persist), Convert.ToDouble(value));
-        }
-        else
-        {
-            if (persist == null)
-            {
-                session = value;
-                persist = value;
-            }
-            else if (type == typeof(int))
-            {
-                session = Convert.ToInt32(session) + Convert.ToInt32(value);
-                persist = Convert.ToInt32(persist) + Convert.ToInt32(value);
-            }
-            else if (type == typeof(long))
-            {
-                session = Convert.ToInt64(session) + Convert.ToInt64(value);
-                persist = Convert.ToInt64(persist) + Convert.ToInt64(value);
-            }
-            else if (type == typeof(float))
-            {
-                session = Convert.ToSingle(session) + Convert.ToSingle(value);
-                persist = Convert.ToSingle(persist) + Convert.ToSingle(value);
-            }
-            else if (type == typeof(double))
-            {
-                session = Convert.ToDouble(session) + Convert.ToDouble(value);
-                persist = Convert.ToDouble(persist) + Convert.ToDouble(value);
-            }
-        }
-        SetSteam(null); // key는 외부에서 전달
-    }
 
-    public object GetSession() => session;
-    public object GetPersist() => persist;
-    public object GetHigh() => persist;
-    public void EndSession() => session = null;
-
-    public void SetSteam(string key)
-    {
-#if STEAM
-        if (isSteam == false || key == null) return;
-        if (type == typeof(long))
-            Steamworks.SteamUserStats.SetStat(key, Convert.ToInt64(persist));
-        else if (type == typeof(int))
-            Steamworks.SteamUserStats.SetStat(key, Convert.ToInt32(persist));
-        else if (type == typeof(float) || type == typeof(double) )
-            Steamworks.SteamUserStats.SetStat(key, Convert.ToSingle(persist));
-#endif
-    }
-}
-
-public static class Records
-{
     static bool isDirty = false;
-    static Dictionary<string, RecordValue> records = new();
+    static Dictionary<string, Records> records = new();
     static readonly string SavePath = Path.Combine(UnityEngine.Application.persistentDataPath, "records.sav");
 
     public static void Model(string key, Type type, bool isHigh, bool isSteam = false)
     {
-        if (!records.ContainsKey(key))
-            records[key] = new RecordValue(type, isHigh, isSteam);
+        if (records.ContainsKey(key) == false)
+            records[key] = new Records(type, isHigh, isSteam);
+        else
+        {
+            var rec = records[key];
+            rec.type = type;
+            rec.ifHigh = isHigh;
+            rec.isSteam = isSteam;
+        }
     }
     public static void Operate(string key, object value)
     {
-        if (records.TryGetValue(key, out var rec))
+        if (records.TryGetValue(key, out var rec) == false)
         {
-            rec.Operate(value);
-            rec.SetSteam(key);
-            isDirty = true;
+            Debug.LogError($"Record with key '{key}' does not exist. Please create it first.");
+            return;
         }
+        if (rec.ifHigh)
+        {
+            if (rec.persist == null) rec.persist = value;
+            else if (rec.type == typeof(int)) rec.persist = Math.Max(Convert.ToInt32(rec.persist), Convert.ToInt32(value));
+            else if (rec.type == typeof(long)) rec.persist = Math.Max(Convert.ToInt64(rec.persist), Convert.ToInt64(value));
+            else if (rec.type == typeof(float)) rec.persist = Math.Max(Convert.ToSingle(rec.persist), Convert.ToSingle(value));
+            else if (rec.type == typeof(double)) rec.persist = Math.Max(Convert.ToDouble(rec.persist), Convert.ToDouble(value));
+        }
+        else
+        {
+            if (rec.persist == null)
+            {
+                rec.session = value;
+                rec.persist = value;
+            }
+            else if (rec.type == typeof(int))
+            {
+                rec.session = Convert.ToInt32(rec.session) + Convert.ToInt32(value);
+                rec.persist = Convert.ToInt32(rec.persist) + Convert.ToInt32(value);
+            }
+            else if (rec.type == typeof(long))
+            {
+                rec.session = Convert.ToInt64(rec.session) + Convert.ToInt64(value);
+                rec.persist = Convert.ToInt64(rec.persist) + Convert.ToInt64(value);
+            }
+            else if (rec.type == typeof(float))
+            {
+                rec.session = Convert.ToSingle(rec.session) + Convert.ToSingle(value);
+                rec.persist = Convert.ToSingle(rec.persist) + Convert.ToSingle(value);
+            }
+            else if (rec.type == typeof(double))
+            {
+                rec.session = Convert.ToDouble(rec.session) + Convert.ToDouble(value);
+                rec.persist = Convert.ToDouble(rec.persist) + Convert.ToDouble(value);
+            }
+        }
+// #if STEAM
+        if (rec.isSteam == false || key == null) return;
+        if (rec.type == typeof(long))
+            Steamworks.SteamUserStats.SetStat(key, Convert.ToInt64(rec.persist));
+        else if (rec.type == typeof(int))
+            Steamworks.SteamUserStats.SetStat(key, Convert.ToInt32(rec.persist));
+        else if (rec.type == typeof(float) || rec.type == typeof(double) )
+            Steamworks.SteamUserStats.SetStat(key, Convert.ToSingle(rec.persist));
+// #endif
+        isDirty = true;
     }
     public static object GetHigh(string key)
     {
-        if (records.TryGetValue(key, out var rec) && rec.IsHigh)
-            return rec.GetHigh();
+        if (records.TryGetValue(key, out var rec) && rec.ifHigh)
+            return rec.persist;
+        Debug.LogWarning($"Record with key '{key}' is not a high record or does not exist.");
         return null;
     }
     public static object GetSession(string key)
     {
-        if (records.TryGetValue(key, out var rec) && rec.IsHigh == false)
-            return rec.GetSession();
+        if (records.TryGetValue(key, out var rec) && rec.ifHigh == false)
+            return rec.session;
+        Debug.LogWarning($"Record with key '{key}' is not a session record or does not exist.");
         return null;
     }
     public static object GetPersist(string key)
     {
-        if (records.TryGetValue(key, out var rec) && rec.IsHigh == false)
-            return rec.GetPersist();
+        if (records.TryGetValue(key, out var rec) && rec.ifHigh == false)
+            return rec.persist;
+        Debug.LogWarning($"Record with key '{key}' is not a persistent record or does not exist.");
         return null;
     }
     public static void EndSession()
     {
-        foreach (var rec in records.Values) rec.EndSession();
+        foreach (var rec in records.Values) rec.session = null;
         isDirty = true;
+        Save();
     }
+
     public static void LoadFromFile()
     {
-        if (!File.Exists(SavePath))
+        if (File.Exists(SavePath) == false)
         {
-            records = new Dictionary<string, RecordValue>();
+            records = new Dictionary<string, Records>();
             return;
         }
         var json = File.ReadAllText(SavePath);
-        Debug.Log($"<color=green>{json}</color>");
+#if UNITY_EDITOR
+        Debug.Log($"records: {json}");
+#endif
         try
         {
             var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
-            records = JsonConvert.DeserializeObject<Dictionary<string, RecordValue>>(json, settings);
+            records = JsonConvert.DeserializeObject<Dictionary<string, Records>>(json, settings);
             isDirty = false;
         }
         catch (Exception e)
         {
             Debug.LogError($"Failed to load records from file: {e.Message}");
-            records = new Dictionary<string, RecordValue>();
+            records = new Dictionary<string, Records>();
         }
     }
     public static void Save()
     {
-        if (!isDirty) return;
+        if (isDirty == false) return;
         isDirty = false;
         var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
         var json = JsonConvert.SerializeObject(records, settings);
@@ -179,9 +173,9 @@ public static class Records
     }
     public static void ShowAll()
     {
-        StringBuilder o = new();
+        StringBuilder sb = new();
         foreach (var kv in records)
-            o.AppendLine($"{kv.Key}: {kv.Value.type} {(kv.Value.IsHigh ? "High" : "Cumulative")} {kv.Value.GetSession()} / {kv.Value.GetPersist()} ");
-        Debug.Log(o);
+            sb.AppendLine($"{kv.Key}: {kv.Value.type} {(kv.Value.ifHigh ? "High" : "Cumulative")} {kv.Value.session} / {kv.Value.persist} ");
+        Debug.Log(sb);
     }
 }
