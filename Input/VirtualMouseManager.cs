4562e888daaca2abd0de8c8bc6f8b17b76c5280b
임세환 <limprovi@line.games>
2025-07-02 18:36:52 +0900

    가상 마우스 관리 기능 개선 및 관련 프리팹 추가

--- Input/VirtualMouseManager.cs
+++ Input/VirtualMouseManager.cs
@@ -1,56 +1,28 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class VirtualMouseManager : MonoBehaviour
 {
-    RectTransform rect;
-    public float speed = 5f;
-    public GameObject virtualCursor;
-    bool wasGamepadActive = false;
-    int screenWidth, screenHeight;
-
+    [SerializeField] RectTransform virtualCursor;
+    
+    Vector2 screenBounds;
+    Vector2 cachedPosition;
 
     void Awake()
     {
-        rect = GetComponent<RectTransform>();
-        rect.anchorMin = Vector2.zero;
-        rect.anchorMax = Vector2.zero;
-        screenWidth = Screen.width;
-        screenHeight = Screen.height;
-        rect.anchoredPosition = new Vector2(screenWidth / 2f, screenHeight / 2f);
-        SetActiveVirtualCursor(false);
+        screenBounds = new Vector2(Screen.width, Screen.height);
     }
 
-    void Update()
+    void LateUpdate()
     {
-        bool gamepadActive = false;
-        var gamepad = Gamepad.current;
-        if (gamepad != null)
-        {
-            Vector2 stick = gamepad.leftStick.ReadValue();
-            Debug.Log("Gamepad Stick: " + stick);
-            if (stick.sqrMagnitude > 0.0001f)
-            {
-                Vector2 newPos = rect.anchoredPosition + speed * Time.deltaTime * stick;
-                rect.anchoredPosition = new Vector2(
-                    Mathf.Clamp(newPos.x, 0, screenWidth),
-                    Mathf.Clamp(newPos.y, 0, screenHeight)
-                );
-                gamepadActive = true;
-            }
-        }
-
-        if (gamepadActive != wasGamepadActive)
-        {
-            if (gamepadActive) SetActiveVirtualCursor(true);
-            else SetActiveVirtualCursor(false);
-        }
-        wasGamepadActive = gamepadActive;
+        ClampCursorToScreen();
     }
 
-    void SetActiveVirtualCursor(bool active)
+
+    void ClampCursorToScreen()
     {
-        virtualCursor.SetActive(active);
-        Cursor.visible = active == false;
+        cachedPosition = virtualCursor.anchoredPosition;
+        cachedPosition.x = Mathf.Clamp(cachedPosition.x, 0, screenBounds.x);
+        cachedPosition.y = Mathf.Clamp(cachedPosition.y, 0, screenBounds.y);
+        virtualCursor.anchoredPosition = cachedPosition;
     }
 }
